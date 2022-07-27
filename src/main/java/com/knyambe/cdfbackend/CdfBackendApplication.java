93b3cb94e396232fe3b43367be870c198f19a456
Kelvin Nyambe <knyambedev@gmail.com>
2022-07-28 00:27:25 +0200

    initial setup

---
+++ src/main/java/com/knyambe/cdfbackend/CdfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.knyambe.cdfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knyambe/cdfbackend/CdfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knyambe.cdfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
