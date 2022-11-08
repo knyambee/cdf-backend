e40e0aa8bdb1e6b020260ffe7b4d79cf13fda180
Kelvin Nyambe <knyambedev@gmail.com>
2022-11-08 15:28:22 +0200

    added keycloak security

--- src/main/java/com/knyambe/cdfbackend/funding/skillsTrainingBursary/models/SkillsTrainingBursary.java
+++ src/main/java/com/knyambe/cdfbackend/funding/skillsTrainingBursary/models/SkillsTrainingBursary.java
@@ -10,7 +10,7 @@ import java.util.Date;
 public class SkillsTrainingBursary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String surname;
 
     private String otherNames;
