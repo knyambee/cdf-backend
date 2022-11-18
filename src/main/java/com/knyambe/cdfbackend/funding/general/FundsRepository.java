70604ac1d2c9fd96e3c1bfc0f1ae3bd0e36f3037
Kelvin Nyambe <knyambedev@gmail.com>
2022-11-18 03:23:37 +0200

    Retrieve recent applications by userid

--- src/main/java/com/knyambe/cdfbackend/funding/general/FundsRepository.java
+++ src/main/java/com/knyambe/cdfbackend/funding/general/FundsRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
+
 @RepositoryRestResource(path = "recentapplications")
 @CrossOrigin
 public interface FundsRepository extends CrudRepository<Funds, Long> {
+    List<Funds> findByUserId(String userId);
 }
