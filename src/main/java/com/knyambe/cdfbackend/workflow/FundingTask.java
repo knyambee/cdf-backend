84f0104f7280e6e5daf41f23e2b6754e9a2d4e2e
Kelvin Nyambe <knyambedev@gmail.com>
2022-11-29 00:49:10 +0200

    added all business processes

--- src/main/java/com/knyambe/cdfbackend/workflow/FundingTask.java
+++ src/main/java/com/knyambe/cdfbackend/workflow/FundingTask.java
@@ -1,6 +1,7 @@
 package com.knyambe.cdfbackend.workflow;
 
 import com.knyambe.cdfbackend.funding.communityProjects.CommunityProjects;
+import com.knyambe.cdfbackend.funding.general.FundApplication;
 import com.knyambe.cdfbackend.security.User;
 import lombok.Data;
 
@@ -11,14 +12,14 @@ public class FundingTask {
     String taskId;
     String taskName;
     Date dateCreated;
-    CommunityProjects taskDetails;
+    FundApplication taskDetails;
     User approver;
 
-    public FundingTask(String id, String name, Date dateCreated, CommunityProjects communityProject, User approver) {
+    public FundingTask(String id, String name, Date dateCreated, FundApplication fundingDetails, User approver) {
         this.taskId = id;
         this.taskName = name;
         this.dateCreated = dateCreated;
-        this.taskDetails = communityProject;
+        this.taskDetails = fundingDetails;
         this.approver = approver;
     }
 }
