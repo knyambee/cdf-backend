dc5083b11ab64e97acd45a7da49e0fe60a393626
Kelvin Nyambe <knyambedev@gmail.com>
2022-12-02 15:34:13 +0200

    Fetch users from keycloak

--- src/main/java/com/knyambe/cdfbackend/security/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.knyambe.cdfbackend.security;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, String> {
-    User findByUsername(String username);
-}
